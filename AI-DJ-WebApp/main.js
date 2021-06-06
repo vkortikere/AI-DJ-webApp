55d9d58d91269bf8dd1391098cd1223c4d052415
vkortikere <69329819+vkortikere@users.noreply.github.com>
2021-06-05 21:21:08 -0400

    Add files via upload

---
+++ AI-DJ-WebApp/main.js
@@ -0,0 +1,20 @@
+
+function preload(){
+song=loadSound("music2.mp3");
+}
+
+ function setup(){
+ canvas = createCanvas(600,500);
+canvas.center();
+    
+video = createCapture(VIDEO);
+video.hide();
+
+}
+
+
+function draw(){
+image(video, 0, 0, 600, 500);
+fill("#FF0000");
+stroke("#FF0000");
+}
