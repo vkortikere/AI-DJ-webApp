f4753931f3cda24c163d4bda530afc506cf38efe
vkortikere <69329819+vkortikere@users.noreply.github.com>
2021-06-06 08:32:34 -0400

    Add files via upload

--- AI-DJ-WebApp/main.js
+++ AI-DJ-WebApp/main.js
@@ -1,3 +1,7 @@
+leftWristX = 0;
+leftWristY= 0;
+rightWristX = 0;
+rightWristY= 0;
 
 function preload(){
 song=loadSound("music2.mp3");
@@ -11,10 +15,23 @@ video = createCapture(VIDEO);
 video.hide();
 
 }
-
+function modelLoaded(){
+    console.log('PoseNet is initialized')
+}
 
 function draw(){
 image(video, 0, 0, 600, 500);
 fill("#FF0000");
 stroke("#FF0000");
+}
+ 
+if(results.length >0){
+    
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX = " + leftWristX+" leftWristY = "+leftWristY)
+
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX = " + rightWristX+" rightWristY = "+rightWristY)
 }
